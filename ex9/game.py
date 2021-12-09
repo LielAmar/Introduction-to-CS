80788983b8ad574a7415075f52f536df8bc5877f
LielAmar <lialiko3@gmail.com>
2021-12-09 21:10:46 +0200

    Final project

--- ex9/game.py
+++ ex9/game.py
@@ -1,3 +1,4 @@
+from car import *
 from board import *
 from helper import *
 
@@ -11,7 +12,7 @@ class Game:
         Initialize a new Game object.
         :param board: An object of type board
         """
-        
+
         self.__board = board
 
     def __single_turn(self):
@@ -30,31 +31,75 @@ class Game:
         you may support additional features, (e.g., hints) as long as they
         don't interfere with the API.
         """
-        # implement your code here (and then delete the next line - 'pass')
-        pass
+
+        print(self.__board)
+        print("===========================")
+
+        user_input = input("""
+            Enter the color of the car you want to move,
+            and the direction you want to move in (e.g., (R,u)):
+            """)
+        if len(user_input) != 3 or user_input[1] != ",":
+            print("""
+                Invalid input. Please enter a valid color and direction.
+            """)
+            return
+
+        color, direction = user_input.split(",")
+        
+        possible_moves = self.__board.possible_moves()
+        
+        if not self.__color_has_move(color, direction, possible_moves):
+            print("""
+                Car not found or recived an invalid/impossible direction.
+                """)
+            return
+
+        board.move_car(color, direction)
+        return
 
     def play(self):
         """
         The main driver of the Game. Manages the game until completion.
         :return: None
         """
-        # implement your code here (and then delete the next line - 'pass')
-        pass
+
+        moves = 0
+
+        while board.cell_content(board.target_location()) == None:
+            moves += 1
+            self.__single_turn()
+
+        print("Well done! You finished the game in {} moves!".format(str(moves)))
+
+
+    # === UTILS ===
+    def __color_has_move(self, color, direction, moves):
+        for move in moves:
+            if move[0] == color and move[1] == direction:
+                return True
+        return False
+
 
 import sys
 
-# TODO check car's name
+VALID_NAMES = ["R","G","W","O","B","Y"]
 
 if __name__== "__main__":
     script, path_to_json = sys.argv
 
-    json = load_json(path_to_json)
-
+    car_data = load_json(path_to_json)
     board = Board()
 
-    # Game(Board()).play()
+    for car in car_data.items():
+        if car[0] in VALID_NAMES:
+            name = car[0]
+            length = car[1][0]
+            location = (car[1][1][0], car[1][1][1])
+            orientation = car[1][2]
 
-    #Your code here
-    #All access to files, non API constructors, and such must be in this
-    #section, or in functions called from this section.
-    pass
+            car = Car(name, length, location, orientation)
+            board.add_car(car)
+
+    game = Game(board)
+    game.play()
